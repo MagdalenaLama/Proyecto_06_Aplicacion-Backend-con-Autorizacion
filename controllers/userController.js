65d47341779299c2ba7fedbf2f10ea3e06f05c53
Magdalena Lama <magdalenalamalyon@gmail.com>
2024-11-14 10:35:27 -0300

    Corrección ruta updateUser  para no retornar password

--- controllers/userController.js
+++ controllers/userController.js
@@ -60,7 +60,7 @@ exports.updateUser = async (req, res) => {
   try {
     const user = await User.findByIdAndUpdate(req.params.id, req.body, {
       new: true,
-    });
+    }).select("-password");
     if (!user) {
       return res.status(404).json({ message: "Usuario no encontrado" });
     }
