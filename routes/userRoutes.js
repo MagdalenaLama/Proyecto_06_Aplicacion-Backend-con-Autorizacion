4cbe921b0ac9f8b3c3eb6de2b53b30cbc8040a9c
Magdalena Lama <magdalenalamalyon@gmail.com>
2024-11-14 09:59:24 -0300

    Rutas de usuario

---
+++ routes/userRoutes.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+const userController = require("../controllers/userController");
+
+router.post("/register", userController.createUser);
+// router.post("/login", userController.userSignIn)
+router.get("/users", userController.getUsers);
+router.get("/users/:id", userController.getUserById);
+router.put("/users/:id", userController.updateUser);
+router.delete("/users/:id", userController.deleteUser);
