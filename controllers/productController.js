01494cb23c658b3aec097fa7f5b3dbf8034b0f0b
Magdalena Lama <magdalenalamalyon@gmail.com>
2024-11-14 09:17:25 -0300

    Product Controller

---
+++ controllers/productController.js
@@ -0,0 +1,55 @@
+const Product = require("../models/Product");
+
+exports.getProducts = async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(500).json({ msg: error.message });
+  }
+};
+
+exports.createProduct = async (req, res) => {
+  const { name, price, stock } = req.body;
+  try {
+    const newProduct = await Product.create({ name, price, stock });
+    res.status(201).json(newProduct);
+  } catch (error) {}
+};
+
+exports.deleteProduct = async (req, res) => {
+  try {
+    const deletedProduct = await Product.findByIdAndDelete(req.params.id);
+    if (!deletedProduct) {
+      return res.status(404).json({ message: "Producto no encontrado" });
+    }
+    res.status(204).send();
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+exports.updateProduct = async (req, res) => {
+  try {
+    const product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    if (!product) {
+      return res.status(404).json({ message: "Producto no encontrado" });
+    }
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+exports.getProduct = async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+    if (!product) {
+      return res.status(404).json({ message: "Producto no encontrado" });
+    }
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
