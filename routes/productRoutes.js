b0f2a7c1dafdb81b0a315a556c3b3e54848319a5
Magdalena Lama <magdalenalamalyon@gmail.com>
2024-11-15 17:47:58 -0300

    Validaciones para rutas de producto

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const productController = require("../controllers/productController");
+const auth = require("../middleware/authorization");
 
+router.post("/", auth.varifyAdminToken, productController.createProduct);
 router.get("/", productController.getProducts);
 router.get("/:id", productController.getProductById);
-router.post("/", productController.createProduct);
-router.delete("/:id", productController.deleteProduct);
-router.put("/:id", productController.updateProduct);
+router.put("/:id", auth.varifyAdminToken, productController.updateProduct);
+router.delete("/:id", auth.varifyAdminToken, productController.deleteProduct);
 
 module.exports = router;
