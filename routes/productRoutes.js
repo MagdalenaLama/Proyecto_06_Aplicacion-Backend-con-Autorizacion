93c089b622dd0a11611048241cd7d61af9a0286e
Magdalena Lama <magdalenalamalyon@gmail.com>
2024-11-24 14:10:55 -0300

    Documentación ruta delete product

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -113,7 +113,72 @@ router.get("/", productController.getProducts);
  */
 
 router.get("/:id", productController.getProductById);
+
+/**
+ * @swagger
+ * /products/{id}:
+ *   put:
+ *     summary: Actualizar información del producto si es que el usuario tiene el rol admin
+ *     tags: [Products]
+ *     security:
+ *       - ApiKeyAuth: []  # Indica que se requiere un token JWT en el encabezado de autorización
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: ID del producto a buscar
+ *         schema:
+ *           type: string
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Product'
+ *     responses:
+ *       200:
+ *         description: El producto se actualizó correctamente
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Product'
+ *       403:
+ *         description: Token requerido o Rol de administrador requerido para esta ruta
+ *       404:
+ *         description: Producto no encontrado
+ *       500:
+ *         description: Error en el servidor
+ */
 router.put("/:id", auth.verifyAdminToken, productController.updateProduct);
+
+/**
+ * @swagger
+ * /products/{id}:
+ *   delete:
+ *     summary: Elimina un producto si es que se envía un token con rol de admin
+ *     tags: [Products]
+ *     security:
+ *       - ApiKeyAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: ID del producto a eliminar
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Product'
+ *     responses:
+ *       204:
+ *         description: El producto se eliminó correctamente
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Product'
+ */
+
 router.delete("/:id", auth.verifyAdminToken, productController.deleteProduct);
 
 module.exports = router;
